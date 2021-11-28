63027f310727abe10b36c6141d41cdbd8f90f843
mahfuj890 <mahfujali490@gmail.com>
2021-11-28 13:26:25 +0600

    about and food items section

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,34 +8,7 @@
 
 })(jQuery);
 
-//Sticky Navbar
-window.addEventListener("scroll", () => {
-  let headerWrapper = document.querySelector(".header_wrapper");
-  headerWrapper.classList.toggle("navbar-fixed", scrollY > 0);
-});
-
-//Nav Menu Resposnive
-var expandButton = document.querySelectorAll(".dropdown_list");
-var dropdown_items = document.querySelectorAll(".dropdown_items_area");
-var dropdownIcon = document.querySelectorAll(".dropdown_icon1");
-expandButton.forEach((e) => {
-  e.addEventListener("click", () => {
-    e.classList.toggle("open");
-  });
-});
-
-//Hamburger Icon
-let hamburger = document.querySelector(".hamburger");
-let navMenu = document.querySelector(".nav_menu_area");
-let headerWrapper = document.querySelector(".header_wrapper");
-
-hamburger.addEventListener("click", mobileMenu);
-function mobileMenu() {
-  hamburger.classList.toggle("hamburger_active");
-  navMenu.classList.toggle("navbar_active");
-  headerWrapper.classList.toggle("header_color_active");
-}
-
+ 
 //Counter
 let visibilityIds = ['#counters_1', '#counters_2', '#counters_3'];
 let counterClass = '.counter';
@@ -57,7 +30,7 @@ function scrollUp() {
   })
 }
 
- 
+
 
 
 // AOS On Page Scroll JS
